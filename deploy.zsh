58be22088364bc7ed8a6428139de2c64c47305fd
Samuel Gonzalez Castillo <samuel@sgc.ink>
2020-07-05 17:13:28 +0200

    Fix bug in deploy script

--- deploy.zsh
+++ deploy.zsh
@@ -2,15 +2,25 @@
 
 DOTFILES=~/repos/dotfiles
 
-ln -s -i $DOTFILES/vimrc ~/.vimrc
-ln -s -i $DOTFILES/gvimrc ~/.gvimrc
-ln -s -i $DOTFILES/vim ~/.vim
+ln -s -f $DOTFILES/vimrc ~/.vimrc
+ln -s -f $DOTFILES/gvimrc ~/.gvimrc
+if [[ -d ~/.vim ]]; then
+	rm ~/.vim
+fi
+ln -s $DOTFILES/vim ~/.vim
 
-ln -s -i $DOTFILES/zshrc ~/.zshrc
-ln -s -i $DOTFILES/zshenv ~/.zshenv
-ln -s -i $DOTFILES/zsh ~/.zsh
+ln -s -f $DOTFILES/zshrc ~/.zshrc
+ln -s -f $DOTFILES/zshenv ~/.zshenv
+if [[ -d ~/.zsh ]]; then
+	rm ~/.zsh
+fi
+ln -s $DOTFILES/zsh ~/.zsh
 
-ln -s -i $DOTFILES/latexmkrc ~/.latexmkrc
+ln -s -f $DOTFILES/latexmkrc ~/.latexmkrc
+if [[ -d ~/Library/texmf/tex/latex/local ]]; then
+	rm ~/Library/texmf/tex/latex/local
+fi
+ln -s $DOTFILES/tex ~/Library/texmf/tex/latex/local
 
 if [[ ! -d ~/.rbin ]]; then
 	mkdir ~/.rbin
