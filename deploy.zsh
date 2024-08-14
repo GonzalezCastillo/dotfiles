78cd17bc9b3887e6c6c8e7f41a6fcc68705abc70
Samuel Gonzalez-Castillo <samuel@sgc.ink>
2024-08-14 20:00:17 +0200

    Update deploy script

--- deploy.zsh
+++ deploy.zsh
@@ -4,23 +4,23 @@ DOTFILES=~/repos/dotfiles
 
 ln -s -f $DOTFILES/vimrc ~/.vimrc
 ln -s -f $DOTFILES/gvimrc ~/.gvimrc
-if [[ -d ~/.vim ]]; then
-	rm ~/.vim
+if [[ -e ~/.vim ]]; then
+	rm -rf ~/.vim
 fi
 ln -s $DOTFILES/vim ~/.vim
 
 ln -s -f $DOTFILES/zshrc ~/.zshrc
 ln -s -f $DOTFILES/zshenv ~/.zshenv
-if [[ -d ~/.zsh ]]; then
-	rm ~/.zsh
+if [[ -e ~/.zsh ]]; then
+	rm -rf ~/.zsh
 fi
 ln -s $DOTFILES/zsh ~/.zsh
 
 TEXMFDIR=~/texmf
 
 ln -s -f $DOTFILES/latexmkrc ~/.latexmkrc
-if [[ -d $TEXMFDIR ]]; then
-	rm $TEXMFDIR
+if [[ -e $TEXMFDIR ]]; then
+	rm -rf $TEXMFDIR
 fi
 ln -s $DOTFILES/sgctex/texmf $TEXMFDIR
 
