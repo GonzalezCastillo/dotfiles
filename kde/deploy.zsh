faf2d74f09ddc1c479e352a4939cfc82eb2e93d5
Samuel Gonzalez-Castillo <samuel@sgc.ink>
2024-08-18 03:20:05 +0200

    kde: Set default apps and kwrite

--- kde/deploy.zsh
+++ kde/deploy.zsh
@@ -13,9 +13,23 @@ cp -f shortcuts.ini ~/.config/kglobalshortcutsrc
 
 cp -f thunderbird.js ~/.thunderbird/*default-default/user.js
 
+cp -f kwrite.ini ~/.config/kwriterc
+
 # The settings in dolphin-sidebar assume that ~ = /home/samuel.
 cp -f dolphin.ini ~/.config/dolphinrc
 cp -f dolphin-view.ini ~/.local/share/dolphin/view_properties/global/.directory
 cp -f dolphin-sidebar.xbel ~/.local/share/user-places.xbel
 cp -f dolphin-toolbar.rc ~/.local/share/kxmlgui5/dolphin/dolphinui.rc
 
+
+# Default apps
+xdg-mime default okularApplication_pdf.desktop application/pdf
+xdg-mime default org.kde.kwrite.desktop text/markdown
+xdg-mime default org.kde.kwrite.desktop text/plain
+xdg-mime default gvim.desktop text/x-tex
+xdg-mime default gvim.desktop text/x-python
+xdg-mime default gvim.desktop text/x-csrc
+xdg-mime default gvim.desktop text/x-chdr
+xdg-mime default gvim.desktop text/x-rust
+xdg-mime default firefox-esr.desktop text/html
+xdg-mime default thunderbird.desktop x-scheme-handler/mailto
