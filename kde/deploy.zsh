7ec917e781d04df1b432fc7c0fb490579b7ae9fd
Samuel Gonzalez-Castillo <samuel@sgc.ink>
2024-08-18 22:23:16 +0200

    kde: Update settings

--- kde/deploy.zsh
+++ kde/deploy.zsh
@@ -4,23 +4,35 @@
 
 unalias cp
 
+safe_mkdir() {
+	if [[ ! -e $1 ]]; then
+		mkdir -p $1
+	fi
+}
+
 cp -f kdeglobals.ini ~/.config/kdeglobals
 cp -f shortcuts.ini ~/.config/kglobalshortcutsrc
 cp -f lockscreen.ini ~/.config/kscreenlockerrc
 cp -f keyboard.ini ~/.config/kxkbrc
+cp -f krunner.ini ~/.config/krunnerrc
+cp -f klaunch.ini ~/.config/klaunchrc
 
 cp -f konsole.ini ~/.config/konsolerc
+safe_mkdir ~/.local/share/konsole
 cp -f konsole-profile.ini ~/.local/share/konsole/Profile1.profile
 cp -f kwrite.ini ~/.config/kwriterc
 
-cp -f thunderbird.js ~/.thunderbird/*default-default/user.js
+cp -f user.js ~/.thunderbird/*default-default/
 
 # The settings in dolphin-sidebar assume that ~ = /home/samuel.
 cp -f dolphin.ini ~/.config/dolphinrc
+safe_mkdir ~/.local/share/dolphin/view_properties/global
 cp -f dolphin-view.ini ~/.local/share/dolphin/view_properties/global/.directory
+safe_mkdir ~/.local/share/kxmlgui5/dolphin
 cp -f dolphin-sidebar.xbel ~/.local/share/user-places.xbel
 cp -f dolphin-toolbar.rc ~/.local/share/kxmlgui5/dolphin/dolphinui.rc
 
+sed -i '1i[Effect-overview]\nBorderActivate=7\n' ~/.config/kwinrc
 
 # Default apps
 xdg-mime default okularApplication_pdf.desktop application/pdf
