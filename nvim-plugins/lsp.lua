789cc263bc2d4fd981f65d44660530e3af7d1d5e
Samuel Gonzalez-Castillo <samuel@sgc.ink>
2025-08-28 22:55:17 +0200

    Update nvim/lsp settings

--- nvim-plugins/lsp.lua
+++ nvim-plugins/lsp.lua
@@ -3,8 +3,7 @@ require("mason").setup{}
 local on_attach = function(client, bufnr)
 	local opts = { remap=false, buffer=bufnr }
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	--vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)	
-	--vim.keymap.set("n", "<leader>rnm", function() vim.lsp.buf.rename() end, opts)	
+	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)	
 	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
 	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 end
